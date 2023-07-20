c32f5771ac083bc26acd780c5133bed09ebd3d1a
emanuel <emanuelsanderscode@gmail.com>
2023-07-20 09:00:27 -0500

    restructured everything to make a bit more sense, Also added a requirements file

--- API/database/int_db.py
+++ API/database/int_db.py
@@ -5,8 +5,12 @@ connection = sqlite3.connect('database.db')
 
 with open('schema.sql') as f:
     connection.executescript(f.read())
-
 cur = connection.cursor()
+cur.execute("INSERT INTO recipes (user, recipe) VALUES (?, ?)",
+            ('Emanuel', 'First Recipe')
+            )
+
+
 
 
 
--- API/run_app.py
+++ API/run_app.py
@@ -1,20 +1,22 @@
-import sqlite3
 from aifc import Error
 
 from flask import Flask, render_template, request
 import sqlite3
 import openAIApi
+from database import insert_functions
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "<h1>Hello World</h1>"
 
 
 def create_connection():
     conn = None
     try:
-        conn = sqlite3.connect('database.db')
+        conn = sqlite3.connect('database/database.db')
         print("Connection to SQLite DB successful")
         return conn
     except Error as e:
@@ -25,22 +27,13 @@ def create_connection():
 
 @app.route('/process', methods=['POST'])
 def process():
-    user = request.form['user']
-    instructions = request.form['instructions']
+    data = request.get_json()
+    user = data['user']
+    instructions = data['instructions']
     recipe = openAIApi.get_recipe(instructions)
+    print(recipe)
 
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    # Insert the user and recipe into the recipes table
-    cursor.execute('''
-        INSERT INTO recipes (user, recipe) VALUES (?, ?)
-    ''', (user, recipe))
-
-
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
+    insert_functions.insert_row(user, recipe)
 
     return 'Data inserted successfully.'
 
