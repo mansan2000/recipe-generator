6807872328e1a299abb05b6fa897a51dbe5a6dd6
emanuel <emanuelsanderscode@gmail.com>
2023-07-21 00:15:32 -0500

    changed folder name and updated requirements

--- src/initDatabase.py
+++ src/initDatabase.py
@@ -4,7 +4,7 @@ from models.models import Recipe
 
 with app.app_context():
     db.create_all()
-    recipe1 = Recipe("mansan", "cook it yourself")
+    recipe1 = Recipe("mansan", "cook it yourself", "cook this for me?")
     print(recipe1.user)
 
     db.session.add_all([recipe1])
